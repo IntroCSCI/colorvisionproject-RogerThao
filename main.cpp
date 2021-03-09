81a1af4033c77267d814de07e4cec35e62043ebb
RogerThao <78010270+RogerThao@users.noreply.github.com>
2021-03-08 23:47:24 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
-//Author: PUT YOUR NAME HERE
+//Author: Roger Thao
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-int main()
-{
+int main(){
+string color;
+string file, text;
+fstream reader;
 
+cout << "Select three colors. \n";
+getline (cin, input);
+cout << "choose a name for the file. \n";
+
+  
+  
   return 0;
 }
