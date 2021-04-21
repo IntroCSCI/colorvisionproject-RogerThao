47fa9e567c445b5c7e15ee26f1a02b54098e88e8
Roger Thao <>
2021-04-21 04:19:03 +0000

    codings

--- main.cpp
+++ main.cpp
@@ -1,38 +1,51 @@
 //Author: Roger Thao
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+
 using namespace std;
 
 int main(){
-string choice;
+vector <string> chosenColors;
+string choice = "";
 string colors, text;
 fstream reader;
 
-do{
+do
+{
 cout << "select three colors. \n";
 getline( cin, colors);
-
-ofstream MyFile ("colors.css");
+fstream MyFile ("colors.css");
 MyFile << colors;
+chosenColors.push_back(colors);
 MyFile.close();
+
+for(int i=0; i < chosenColors.size(); i++)
+{
+  cout << "The chosen colors are: " << colors << endl;
+}
 
 cout << "Open the file marked colors.css \n";
 getline (cin, text);  
 reader.open(text,ios::in );
-  if (reader.is_open() ){
-    while (!reader.eof() ){
+  if(reader.is_open() )
+    {
+    while(!reader.eof() )
+      {
       getline( reader, text);
       cout << colors << endl;
       reader.close();
     }
-  }else {
+  }else 
+    {
     cout << "cannot open file. \n";
   }
 
 cout << "select more colors (yes/no)? \n";
 cin >> choice;
 cin.ignore();
-}while (choice != "no");
+}while(choice != "no");
   
   return 0;
 }
